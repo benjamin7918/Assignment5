e144e4b9bc5791579d04531f10deaa1737ff580d
Benjamin Coon <bc475@byu.edu>
2021-03-10 18:55:19 -0700

    Test

--- Pages/Buy.cshtml.cs
+++ Pages/Buy.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Assignment5.Infrastructure;
+using Assignment5.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,36 @@ namespace Assignment5.Pages
 {
     public class BuyModel : PageModel
     {
-        public void OnGet()
+        private IBookRepository repository;
+        
+        //Constructor
+        public BuyModel (IBookRepository repo)
         {
+            repository = repo;
+        }
+
+        //Properties
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+
+        //Methods
+        public void OnGet(string retunUrl)
+        {
+            ReturnUrl = ReturnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long bookId, string returnUrl)
+        {
+            Project project = repository.Projects.FirstOrDefault(p => p.BookId == bookId);
+
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+
+            Cart.AddItem(project, 1);
+
+            HttpContext.Session.SetJson("cart", Cart);
+
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
